9c7843cdce94c07ce0e055ceb3287e5be7731c6b
NhungTran <nhungtran410@gmail.com>
2021-12-30 00:10:28 +0700

    Init repository

---
+++ selenium/javaBasic/Topic_01_Test_Java_console.java
@@ -0,0 +1,12 @@
+package javaBasic;
+
+public class Topic_01_Test_Java_console {
+
+	public static void main(String[] args) {
+		String projectPath = System.getProperty("user.dir");
+		System.out.print(projectPath);
+
+		
+	}
+
+}
